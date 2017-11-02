0b1e82a21a5721c2e1e88a22ed20321903c2e2d2
jwolfe890 <jwolfe890@gmail.com>
2017-11-02 01:30:16 -0400

    added seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
 
 serialize :found_lists
-
-  validates :email, :uniqueness => {:allow_blank => true}
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
